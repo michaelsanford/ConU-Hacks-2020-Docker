d2704d16d0de1514742b2ea5e2f06cfe1e98607e
Michael Sanford <michaelsanford@users.noreply.github.com>
2020-01-22 23:45:04 -0500

    Enhance builder, fix Go build

--- build.ps1
+++ build.ps1
@@ -1,8 +1,13 @@
-Set-Location '${PSScriptRoot}\Project A'
-docker build -t . project-a
+Write-Host -ForegroundColor Green -BackgroundColor Black "Building Project A"
+Set-Location ${PSScriptRoot}\'Project A'
+docker build . -t project-a
+Write-Host "...done!"
 
-Set-Location '${PSScriptRoot}\Project B'
-docker build -t . project-b
+Write-Host -ForegroundColor Green -BackgroundColor Black "Building Project B"
+Set-Location ${PSScriptRoot}\'Project B'
+docker build . -t project-b
+Write-Host "...done!"
 
+Write-Host -ForegroundColor Green -BackgroundColor Black "Bringing up Stack..."
 Set-Location ${PSScriptRoot}
 docker-compose up
