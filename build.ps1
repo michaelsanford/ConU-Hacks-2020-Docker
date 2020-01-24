40a3a581795833affbf1fa2b594a30dcce4047d9
Michael Sanford <michaelsanford@users.noreply.github.com>
2020-01-23 23:16:15 -0500

    Add switch to detach composer

--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
+param(
+    [switch] $d
+)
+
 Write-Host -ForegroundColor Green -BackgroundColor Black "Building Project A"
 Set-Location ${PSScriptRoot}\'Project A'
 docker build . -t project-a
@@ -10,4 +14,10 @@ Write-Host "...done!"
 
 Write-Host -ForegroundColor Green -BackgroundColor Black "Bringing up Stack..."
 Set-Location ${PSScriptRoot}
-docker-compose up
+
+if ($d -eq $true)
+{
+    docker-compose up --detach
+} else {
+    docker-compose up
+}
